92405251d2d850fc1de0deb1c6526c8cfe2810bb
Nadeeshan Jayawardana <nadeeshan.jayawardana@pearson.com>
2022-05-27 00:22:21 +0530

    first commit

---
+++ Planets/PlanetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanetsApp.swift
+//  Planets
+//
+//  Created by Nadeeshan Jayawardana on 2022-05-27.
+//
+
+import SwiftUI
+
+@main
+struct PlanetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
